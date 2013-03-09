2420e3b16c47ff47eda42e5719a934c88e5a73e7
Oliver Paulus <oliver@code-project.org>
2013-03-09 20:24:34 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WenigerPlastik::Application.config.secret_token = 'fe69fc2f309ef3ef804ed781f2b425b8abf90ac60b89efd41f3b6235ec1caebd66bdd7698cf86c1414a9021b8cfb5ccc91d3b867bccfb32e68a074301b14fb85'
